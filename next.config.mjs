194d326cbfda0743260b859897b05e7c7eacb729
Chaurasia <achaurasia@satschel.com>
2026-01-08 14:37:14 +0530

    setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/vinayka_rugs',
     images: {
         unoptimized: true,
         remotePatterns: [
